26b5b3800379de57e873af87e499826ccae8570a
felipericardoevaristo <felipericardo@ci.ufpb.br>
2021-12-13 10:40:10 -0300

    validação para cpf invalidado

--- src/main/java/com/felipe/helpdesk/resources/exceptions/ResourceExceptionalHandler.java
+++ src/main/java/com/felipe/helpdesk/resources/exceptions/ResourceExceptionalHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice                   /*manipulador de exceções*/
 public class ResourceExceptionalHandler {
@@ -43,5 +44,14 @@ public class ResourceExceptionalHandler {
         }
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<StandardError> validationErrorsConstraint(ConstraintViolationException ex,
+                                                          HttpServletRequest request){
+
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Validation error", "número do registro de contribuinte individual brasileiro (CPF) inválido", request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
